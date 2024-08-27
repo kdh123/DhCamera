3d276eaf3d6136751a062987c5b6f12e6b39037e
dohyun <dnlwkem39@gmail.com>
2024-08-27 19:14:54 +0900

    update version

--- dhcamera/build.gradle.kts
+++ dhcamera/build.gradle.kts
@@ -12,11 +12,11 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.kdh123"
                 artifactId = "DhCamera"
-                version = "1.0.0-alpha03"
+                version = "1.0.0-alpha04"
 
                 pom {
-                    name.set("DhCamera-1.0.0-alpha03")
-                    description.set("DhCamera-1.0.0-alpha03")
+                    name.set("DhCamera-1.0.0-alpha04")
+                    description.set("DhCamera-1.0.0-alpha04")
                     url.set("https://github.com/kdh123/DhCamera.git")
 
                     licenses {
